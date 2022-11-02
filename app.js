e3c11f37d3b0f8be15de33a1a9dc8fc4675832f8
Alex Aaron <alexaaron@Operations-MacBook-Air.local>
2022-11-01 19:21:28 -0500

    created initial structure of app.js and index.ejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(__dirname + '/public/styles'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000');
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {} );
+});
