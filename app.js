81f261f5d8af87cfd83530737f9e0464f63ac12e
Alex Aaron <alexaaron@Operations-MacBook-Air.local>
2022-11-03 09:27:43 -0500

    created partials folder in views to hold header and footer files

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index', {} );
 });
+
+app.get('/contact', (req, res) => {
+
+});
+
+app.get('/about', (req, res) => {
+
+});
